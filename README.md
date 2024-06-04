7a88da2da0b6549827925a6b651ef3156e110d9b
evgeny1754 <evgeny1754@yandex.ru>
2024-06-04 06:17:12 +0300

    final project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Проектная работа Mesto
 
+# Адрес проектной работы:
+https://github.com/evgeny1754/mesto-project-ff.git
+
+# Автор работы: Евгений Щербаков
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(card, deleteCard){
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+  cardElement.querySelector('.card__title').textContent = card.name;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+  return cardElement;
+}
 
-// @todo: Функция удаления карточки
+// Функция удаления карточки
+function deleteCard(evt) {
+  const eventTarget = evt.target;
+  const cardElement = eventTarget.closest('.card');
+  cardElement.remove();
+}
 
-// @todo: Вывести карточки на страницу
+// Вывести карточки на страницу
+initialCards.forEach((card) => {
+  const cardElement = createCard(card, deleteCard);
+  placesList.append(cardElement);
+})
